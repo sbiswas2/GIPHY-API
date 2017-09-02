2df2759180bbb823060f47410763946b95cfd7e9
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-09-02 18:04:51 -0500

    created layout and style

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,15 @@
+// topics - create an array of strings
+
+
+// use for loop to create buttons from the array of strings
+
+
+// when a button is clicked, API should grab 10 static gifs - display image
+	// display rating of gif
+
+
+// when user clicks the gif, it will animate
+// when user clicks the gif again, it will stop
+
+
+// user input from form will be added to array, then a function that creates new button
